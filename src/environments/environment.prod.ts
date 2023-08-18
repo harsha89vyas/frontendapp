d14444baa334ded2393e27419f91802cbb560782
Harsha Palli <harsha89vyas@gmail.com>
2023-08-17 20:49:17 -0400

    wss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  webSocketUrl: 'ws://backendapp-lang-c4717859eb2b.herokuapp.com/ws',
+  webSocketUrl: 'wss://backendapp-lang-c4717859eb2b.herokuapp.com/ws',
   apiUrl: 'https://backendapp-lang-c4717859eb2b.herokuapp.com/api',
-  humanWebSocketUrl: 'ws://backendapp-lang-c4717859eb2b.herokuapp.com/human',
+  humanWebSocketUrl: 'wss://backendapp-lang-c4717859eb2b.herokuapp.com/human',
 };
